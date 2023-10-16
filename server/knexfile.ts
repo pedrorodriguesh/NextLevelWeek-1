419026ce555186ce48e9f4d9c1dfdf3b36d7e4f6
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-16 18:14:52 -0300

    first table done

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,4 +1,5 @@
 import type { Knex } from "knex";
+import path from 'path'
 
 // Update with your config settings.
 
@@ -6,42 +7,13 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3"
-    }
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    useNullAsDefault: true,
   },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
-
 };
 
 module.exports = config;
