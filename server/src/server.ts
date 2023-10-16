4b7267c287744e673092f80dd60a49073c61153f
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-16 16:34:55 -0300

    start building api

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,42 @@ import express from "express";
 
 const app = express();
 
+/* 
+  GET => buscar UMA ou MAIS informações do back-end
+  POST => criar uma nova informação no back-end
+  PUT => atualizar uma informação existente no back-end
+  DELETE => deletar uma informação do back-end
+*/
+
+// Request Param => Parâmetros que vem na própria rota que identificam um recurso.
+
+const users = [
+  'diego',
+  'cleiton',
+  'robson',
+  'pedro'
+]
+
 app.get("/users", (request, response) => {
   console.log("Listagem de Usuários");
 
-  response.json([
-    'diego',
-    'cleiton',
-    'robson',
-    'pedro'
-  ]);
+  return response.json(users);
 });
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id]
+
+  return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego',
+    email: 'diego@email.com'
+  }
+
+  return response.json(user)
+})
 
 app.listen(3333);
