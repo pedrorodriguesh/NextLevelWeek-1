3182fedc8c66055a0a3e34991067cf3a828e4aaa
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-17 16:51:41 -0300

    some changes on routes

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -2,35 +2,39 @@ import knex from "../database/connection";
 import { Request, Response } from "express";
 
 class PointsController {
-  async show(request: Request, response: Response){
-    const { id } = request.params
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
 
-    const point = await knex('points').where('id', id).first()
+    const point = await knex("points").where("id", id).first();
 
-    if(!point){
-      return response.status(400).json({ message: "Point not found."})
+    if (!point) {
+      return response.status(400).json({ message: "Point not found." });
     }
 
-    return response.json(point)
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id)
+      .select('items.title');
+
+    return response.json({ point, items });
   }
 
   async create(request: Request, response: Response) {
-    const { name, email, whatsapp, latitute, longitude, city, uf, items } = request.body;
-
-    const trx = await knex.transaction();
+    const { name, email, whatsapp, latitute, longitude, city, uf, items } =
+      request.body;
 
     const point = {
-        image: "image-fake",
-        name,
-        email,
-        whatsapp,
-        latitute,
-        longitude,
-        city,
-        uf,
-      }
+      image: "image-fake",
+      name,
+      email,
+      whatsapp,
+      latitute,
+      longitude,
+      city,
+      uf,
+    };
 
-    const insertedIds = await trx("points").insert(point);
+    const insertedIds = await knex("points").insert(point);
 
     const point_id = insertedIds[0];
 
@@ -41,13 +45,14 @@ class PointsController {
       };
     });
 
-    await trx("point_items").insert(pointItems);
+    await knex("point_items").insert(pointItems);
 
     return response.json({
-        id: point_id,
-        ...point
+      id: point_id,
+      ...point,
     });
   }
 }
 
 export default PointsController;
+ 
