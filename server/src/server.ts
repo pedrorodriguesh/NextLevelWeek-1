25bf4aa7cde78a9b8d05fa5263fcd092e58b7ddd
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-17 15:44:38 -0300

    items image upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 // start do servidor.
 import express from "express";
 import routes from './routes'
+import path from 'path'
 
 const app = express();
+
 app.use(express.json())
 app.use(routes)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))) // express.static para servir as imagens estáticas da aplicação.
 
 app.listen(3333);
