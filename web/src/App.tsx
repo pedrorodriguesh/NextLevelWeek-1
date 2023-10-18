baef2db47492aa3c74c5c649134a19986757afce
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-18 17:39:57 -0300

    start front end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-import Header from "./Header";
-
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function handleButtonClick() {
-    setCounter(counter + 1)
-  }
-
   return (
-    <div>
-      <Header title={`Contador: ${counter}`}/>
-      <button type="button" onClick={handleButtonClick}> Aumentar </button>
-    </div>    
+    <h1>Hello World</h1>
   );
 }
 
