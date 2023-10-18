c9b2e0949219a772145601a8a819c7e71727a1cd
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-18 16:14:47 -0300

    comments backedn

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -42,7 +42,8 @@ class PointsController {
       request.body;
 
     const trx = await knex.transaction()
-      
+    
+    // ### constante para deixar o código mais organizado, alocando todos os dados recebidos do request. 
     const point = {
       image: "https://images.unsplash.com/photo-1542838132-92c53300491e?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=400",
       name,
@@ -54,7 +55,7 @@ class PointsController {
       uf,
     };
 
-    const insertedIds = await trx("points").insert(point);
+    const insertedIds = await trx("points").insert(point); // criando registro na tabela points. 
 
     const point_id = insertedIds[0];
 
