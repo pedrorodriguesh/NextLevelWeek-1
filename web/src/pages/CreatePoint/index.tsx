1656b80dc748744dc9d9cf90f48b6c97f9311abb
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-19 16:23:38 -0300

    start of thursday - map integration

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {FiArrowLeft} from 'react-icons/fi'
+import { FiArrowLeft } from "react-icons/fi";
 
 import "./styles.css";
 import logo from "../../assets/logo.svg";
@@ -12,95 +12,97 @@ const CreatePoint = () => {
         <img src={logo} alt="ecoleta logo" />
 
         <Link to="/">
-            <FiArrowLeft />
-            Voltar para Home
+          <FiArrowLeft />
+          Voltar para Home
         </Link>
       </header>
 
       <form>
-        <h1>Cadastro do <br/> ponto de coleta</h1>
+        <h1>
+          Cadastro do <br /> ponto de coleta
+        </h1>
 
         <fieldset>
-            <legend>
-                <h2>Dados</h2>
-            </legend>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
 
             <div className="field">
-                <label htmlFor="name">Nome da entidade</label>
-                <input type="text" name="name" id="name" />
+              <label htmlFor="whataspp">Whatsapp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
             </div>
-
-            <div className="field-group">
-            
-            <div className="field">
-                <label htmlFor="email">E-mail</label>
-                <input type="email" name="email" id="email" />
-            </div>
-
-            <div className="field">
-                <label htmlFor="whataspp">Whatsapp</label>
-                <input type="text" name="whatsapp" id="whatsapp" />
-            </div>
-
-            </div>
+          </div>
         </fieldset>
 
         <fieldset>
-            <legend>
-                <h2>Endereço</h2>
-                <span>Selecione o endereço no mapa</span>
-            </legend>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
 
-            <div className="field-group">
-                <div className="field">
-                    <label htmlFor="uf">Estado (UF)</label>
-                    <select name="uf" id="uf">
-                        <option value="0">Selecione uma UF</option>
-                    </select>
-                </div>
-
-                <div className="field">
-                    <label htmlFor="city">Cidade</label>
-                    <select name="city" id="city">
-                        <option value="0">Selecione uma cidade</option>
-                    </select>
-                </div>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
             </div>
 
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
 
         <fieldset>
-            <legend>
-                <h2>Itens de Coleta</h2>
-                <span>Selecione 1 ou mais itens abaixo</span>
-            </legend>
+          <legend>
+            <h2>Itens de Coleta</h2>
+            <span>Selecione 1 ou mais itens abaixo</span>
+          </legend>
 
-            <ul className="items-grid">
-                <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                    <span>Óleo de Cozinha</span>
-                </li>
-                <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                    <span>Óleo de Cozinha</span>
-                </li>
-                <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                    <span>Óleo de Cozinha</span>
-                </li>
-                <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                    <span>Óleo de Cozinha</span>
-                </li>
-                <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                    <span>Óleo de Cozinha</span>
-                </li>
-                <li>
-                    <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                    <span>Óleo de Cozinha</span>
-                </li>
-            </ul>
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img
+                src="http://localhost:3333/uploads/lampadas.svg"
+                alt="teste"
+              />
+              <span>Óleo de Cozinha</span>
+            </li>
+          </ul>
         </fieldset>
 
         <button type="submit">Cadastrar ponto de coleta</button>
