cd559cd71ab11fc11ffa5987a6a0082e41fa634b
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-24 12:17:16 -0300

    home page building

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar, View } from "react-native";
+import {} from 'expo'
 
-import Home from './src/pages/Home'
+import Home from "./src/pages/Home";
+
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  
+
   return (
     <>
-      <StatusBar barStyle={"dark-content"} backgroundColor="transparent" translucent/>
+      <StatusBar
+        barStyle={"dark-content"}
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
     </>
   );
-};
+}
