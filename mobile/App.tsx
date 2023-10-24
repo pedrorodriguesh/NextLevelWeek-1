eb9fde7332eff7b3ee097dd3851669d84dee1053
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-24 14:04:21 -0300

    home page done

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,11 +7,16 @@ import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
 import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
+
+  const [fontsLoaded, fontError] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
     Ubuntu_700Bold
   })
+
+  if(!fontsLoaded && !fontError) {
+    return null
+  }
 
   
 
