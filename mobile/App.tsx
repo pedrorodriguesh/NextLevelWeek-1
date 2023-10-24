a4c01a2e2a3db5e62d15702ebff1d03110cb7c45
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-24 11:46:20 -0300

    starting home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
+
+import Home from './src/pages/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle={"dark-content"} backgroundColor="transparent" translucent/>
+      <Home />
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
