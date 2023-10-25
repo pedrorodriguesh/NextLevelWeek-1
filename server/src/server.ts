f68a108211700d7efc4c8807c8cf4a863e18dc1e
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-25 19:21:01 -0300

    validando dados com celebrate

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import routes from "./routes";
 import path from "path";
 import cors from "cors";
 
+import { errors } from "celebrate";
+
 const app = express();
 
 app.use(cors());
@@ -11,5 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads"))); // express.static para servir as imagens estáticas da aplicação.
+
+app.use(errors());
 
 app.listen(3333);
