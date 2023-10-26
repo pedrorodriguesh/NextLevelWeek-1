ff67571dfcc03c091a0fa90c2253584ec923c77b
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-26 20:26:39 -0300

    comments

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -5,9 +5,9 @@ import path from "path";
 
 export default {
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, "..", "..", "uploads"),
+    destination: path.resolve(__dirname, "..", "..", "uploads"), // onde o arquivo vai ficar salvo
     filename(request, file, callback) {
-      const hash = crypto.randomBytes(6).toString("hex");
+      const hash = crypto.randomBytes(6).toString("hex"); // hash com um código hexadecimal para garantir que o arquivo não tenha o mesmo nome
       const fileName = `${hash}-${file.originalname}`;
 
       callback(null, fileName);
