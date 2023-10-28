63e0b1aafa4e190a8eed7f8867e782099a199e43
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-28 14:46:33 -0300

    back-end finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import routes from "./routes";
 import path from "path";
 import cors from "cors";
 
-import { errors } from "celebrate";
+import { errors } from "celebrate"; 
 
 const app = express();
 
